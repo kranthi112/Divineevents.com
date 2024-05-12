23f74800c8a7e8efc711148dc9c1a5ca62f1a631
kranthi112 <kranthi.deee@gmail.com>
2024-05-12 21:44:18 +0530

    Add files via upload

---
+++ web/brielle6056.hocoos.com/brielle6056.hocoos.com/_nuxt/js/lazy_widgets_slider_t1.06f446a8ba682a759621.js
@@ -0,0 +1,346 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+    [145], {
+        1307: function(t, e, n) {
+            "use strict";
+            n.r(e);
+            var d = n(6),
+                r = n(2),
+                l = n.n(r),
+                o = n(33),
+                m = n(310),
+                c = d.componentFactory.create({
+                    props: {
+                        widget: l.a.ofType().required,
+                        widgetIndex: l.a.ofType().default(0),
+                        settings: l.a.ofType().required,
+                        isEnabledAnimation: l.a.ofType().default(!1)
+                    },
+                    render(t) {
+                        var e;
+                        return t(o.kb, {
+                            class: "slider slider-t1 slider-t1-v1",
+                            attrs: {
+                                settings: this.settings
+                            }
+                        }, [t(o.jb, {
+                            attrs: {
+                                settings: this.settings
+                            }
+                        }, [t("div", {
+                            class: "kmb-widget-content kmb-widget-slider-content"
+                        }, [t(o.V, {
+                            attrs: {
+                                indicators: !0,
+                                items: this.settings.items,
+                                settings: {
+                                    itemsToShow: 1
+                                },
+                                uiSettings: this.settings.sliderSettings
+                            },
+                            class: "kmb-slider-inside-navigation kmb-site-slider-pagination-md-bottom kmb-slider-inside-navigation-opacity-circle kmb-widget-slider-inside-space-row"
+                        }, [null === (e = this.settings.items) || void 0 === e ? void 0 : e.map(((e, n) => t(o.W, {
+                            attrs: {
+                                index: n
+                            },
+                            key: e.key || n,
+                            class: "kmb-slider-item-h-auto"
+                        }, [t(o.c, {
+                            attrs: {
+                                image: e.image,
+                                widget: this.widget,
+                                settings: this.settings
+                            },
+                            class: "kmb-full-bg-item kmb-widget-item-content kmb-widget-bg-image-with-overlay h-full"
+                        }, [t(o.e, {
+                            attrs: {
+                                settings: this.settings,
+                                contentClassNames: "flex justify-center"
+                            }
+                        }, [t("div", {
+                            class: "kmb-widget-container lg-py-18"
+                        }, [t(m.a, {
+                            attrs: {
+                                widgetIndex: this.widgetIndex,
+                                isEnabledAnimation: this.isEnabledAnimation,
+                                cardType: "clear",
+                                item: e
+                            }
+                        })])])])])))])])])])
+                    }
+                });
+            e.default = c
+        },
+        1311: function(t, e, n) {
+            "use strict";
+            n.r(e);
+            var d = n(6),
+                r = n(2),
+                l = n.n(r),
+                o = n(33),
+                m = n(310),
+                c = d.componentFactory.create({
+                    props: {
+                        widget: l.a.ofType().required,
+                        widgetIndex: l.a.ofType().default(0),
+                        settings: l.a.ofType().required,
+                        isEnabledAnimation: l.a.ofType().default(!1)
+                    },
+                    render(t) {
+                        var e;
+                        return t(o.kb, {
+                            class: "slider slider-t1 slider-t1-v13",
+                            attrs: {
+                                settings: this.settings
+                            }
+                        }, [t(o.jb, {
+                            attrs: {
+                                settings: this.settings
+                            }
+                        }, [t("div", {
+                            class: "kmb-widget-content kmb-widget-slider-content"
+                        }, [t(o.V, {
+                            attrs: {
+                                indicators: !0,
+                                items: this.settings.items,
+                                settings: {
+                                    itemsToShow: 1
+                                },
+                                uiSettings: this.settings.sliderSettings
+                            },
+                            class: "kmb-slider-inside-navigation kmb-site-slider-pagination-md-bottom kmb-widget-slider-inside-space-row"
+                        }, [null === (e = this.settings.items) || void 0 === e ? void 0 : e.map(((e, n) => t(o.W, {
+                            attrs: {
+                                index: n
+                            },
+                            key: e.key || n,
+                            class: "kmb-slider-item-h-auto"
+                        }, [t(o.c, {
+                            attrs: {
+                                image: e.image,
+                                widget: this.widget,
+                                settings: this.settings
+                            },
+                            class: "kmb-full-bg-item kmb-widget-item-content kmb-widget-bg-image-with-overlay h-full"
+                        }, [t(o.e, {
+                            attrs: {
+                                settings: this.settings,
+                                contentClassNames: "flex justify-center"
+                            }
+                        }, [t("div", {
+                            class: "kmb-widget-container py-10"
+                        }, [t(m.a, {
+                            attrs: {
+                                widgetIndex: this.widgetIndex,
+                                isEnabledAnimation: this.isEnabledAnimation,
+                                cardType: "clear",
+                                item: e
+                            }
+                        })])])])])))])])])])
+                    }
+                });
+            e.default = c
+        },
+        1312: function(t, e, n) {
+            "use strict";
+            n.r(e);
+            var d = n(6),
+                r = n(2),
+                l = n.n(r),
+                o = n(33),
+                m = n(310),
+                c = d.componentFactory.create({
+                    props: {
+                        widget: l.a.ofType().required,
+                        widgetIndex: l.a.ofType().default(0),
+                        settings: l.a.ofType().required,
+                        isEnabledAnimation: l.a.ofType().default(!1)
+                    },
+                    render(t) {
+                        var e;
+                        return t(o.kb, {
+                            class: "slider slider-t1 slider-t1-v14",
+                            attrs: {
+                                settings: this.settings
+                            }
+                        }, [t(o.jb, {
+                            attrs: {
+                                settings: this.settings
+                            }
+                        }, [t("div", {
+                            class: "kmb-widget-content kmb-widget-slider-content"
+                        }, [t(o.V, {
+                            attrs: {
+                                indicators: !0,
+                                items: this.settings.items,
+                                settings: {
+                                    itemsToShow: 1
+                                },
+                                uiSettings: this.settings.sliderSettings
+                            },
+                            class: "kmb-widget-slider-inside-space-row kmb-slider-inside-navigation-opacity-circle kmb-widget-bg kmb-widget-no-radius py-10"
+                        }, [null === (e = this.settings.items) || void 0 === e ? void 0 : e.map(((e, n) => t(o.W, {
+                            attrs: {
+                                index: n
+                            },
+                            key: e.key || n,
+                            class: "kmb-slider-item-h-auto kmb-flex-centered-position"
+                        }, [t("div", {
+                            class: "kmb-widget-md-height-container"
+                        }, [t("div", {
+                            class: "flex items-center justify-center"
+                        }, [t("div", {
+                            class: "kmb-widget-container"
+                        }, [t(m.a, {
+                            attrs: {
+                                widgetIndex: this.widgetIndex,
+                                isEnabledAnimation: this.isEnabledAnimation,
+                                cardType: "clear",
+                                item: e
+                            }
+                        })])])])])))])])])])
+                    }
+                });
+            e.default = c
+        },
+        1313: function(t, e, n) {
+            "use strict";
+            n.r(e);
+            var d = n(6),
+                r = n(2),
+                l = n.n(r),
+                o = n(33),
+                m = n(310),
+                c = d.componentFactory.create({
+                    props: {
+                        widget: l.a.ofType().required,
+                        widgetIndex: l.a.ofType().default(0),
+                        settings: l.a.ofType().required,
+                        isEnabledAnimation: l.a.ofType().default(!1)
+                    },
+                    render(t) {
+                        var e;
+                        return t(o.kb, {
+                            class: "slider slider-t1 slider-t1-v2 kmb-widget-value-main-bg",
+                            attrs: {
+                                settings: this.settings
+                            }
+                        }, [t(o.jb, {
+                            attrs: {
+                                settings: this.settings
+                            }
+                        }, [t("div", {
+                            class: "kmb-widget-content kmb-widget-slider-content"
+                        }, [t(o.V, {
+                            attrs: {
+                                indicators: !0,
+                                items: this.settings.items,
+                                settings: {
+                                    itemsToShow: 1
+                                },
+                                uiSettings: this.settings.sliderSettings
+                            },
+                            class: "kmb-widget-slider-inside-space-row kmb-mini-banner-bg kmb-widget-no-radius py-10"
+                        }, [null === (e = this.settings.items) || void 0 === e ? void 0 : e.map(((e, n) => t(o.W, {
+                            attrs: {
+                                index: n
+                            },
+                            key: e.key || n
+                        }, [t("div", {
+                            class: "kmb-widget-height-container"
+                        }, [t("div", {
+                            class: "flex items-center justify-center"
+                        }, [t("div", {
+                            class: "kmb-widget-container"
+                        }, [t(m.a, {
+                            attrs: {
+                                widgetIndex: this.widgetIndex,
+                                isEnabledAnimation: this.isEnabledAnimation,
+                                cardType: "clear",
+                                item: e
+                            }
+                        })])])])])))])])])])
+                    }
+                });
+            e.default = c
+        },
+        310: function(t, e, n) {
+            "use strict";
+            var d = n(6),
+                r = n(2),
+                l = n.n(r),
+                o = n(5),
+                m = n.n(o),
+                c = n(0),
+                w = n(33),
+                h = d.componentFactory.create({
+                    name: "SliderItemContent",
+                    props: {
+                        item: l.a.ofAny().required,
+                        widgetIndex: l.a.ofType().required,
+                        cardType: l.a.ofType().default("right"),
+                        isEnabledButtons: l.a.ofType().default(!0),
+                        isEnabledDivider: l.a.ofType().default(!1),
+                        isEnabledAnimation: l.a.ofType().default(!1),
+                        isEnabledSecondaryButton: l.a.ofType().default(!0)
+                    },
+                    methods: {
+                        renderContent(t, e) {
+                            return t(w.n, {
+                                attrs: {
+                                    settings: this.item,
+                                    isEnabledAnimation: this.isEnabledAnimation
+                                },
+                                class: m()(e)
+                            }, [t(w.fb, {
+                                attrs: {
+                                    widgetIndex: this.widgetIndex,
+                                    value: this.item.title,
+                                    isEnabledDivider: this.isEnabledDivider
+                                },
+                                class: "kmb-slider-title"
+                            }), t(w.q, {
+                                attrs: {
+                                    value: this.item.description
+                                },
+                                class: "kmb-slider-description"
+                            }), t(w.i, {
+                                class: "kmb-widget-btn-group-center flex justify-center"
+                            }, [t(w.h, {
+                                attrs: {
+                                    value: this.item.primaryButton,
+                                    elementType: c.Qd.Primary
+                                },
+                                class: "kmb-widget-primary-btn"
+                            }), this.isEnabledSecondaryButton && t(w.h, {
+                                attrs: {
+                                    value: this.item.secondaryButton,
+                                    elementType: c.Qd.Secondary,
+                                    classNames: "kmb-widget-secondary-btn"
+                                }
+                            })])])
+                        }
+                    },
+                    render(t) {
+                        return "right" === this.cardType ? t("div", {
+                            class: "kmb-widget-slider-right-title-main-block z-10"
+                        }, [t("div", {
+                            class: "container"
+                        }, [this.renderContent(t, "kmb-slide-content-box kmb-widget-mini-banner-bg lg-w-1-2 md-w-3-5 w-3-4 px-8 lg-py-24 py-16 shadow-md")])]) : "left" === this.cardType ? t("div", {
+                            class: "kmb-widget-slider-left-title-main-block z-10"
+                        }, [t("div", {
+                            class: "container"
+                        }, [this.renderContent(t, "kmb-slide-content-box kmb-widget-mini-banner-bg lg-w-1-2 md-w-3-5 w-4-5 px-8 lg-py-24 py-16 shadow-md")])]) : "center" === this.cardType ? t("div", {
+                            class: "kmb-widget-slider-center-title-main-block relative z-10"
+                        }, [t("div", {
+                            class: "container mx-auto"
+                        }, [this.renderContent(t, "kmb-slide-content-box kmb-widget-mini-banner-bg mx-auto md-p-16 p-12")])]) : "clear" === this.cardType ? t("div", {
+                            class: "kmb-slide-content w-full text-center z-10"
+                        }, [t("div", {
+                            class: "kmb-widget-default-info"
+                        }, [this.renderContent(t, "kmb-widget-titles-info")])]) : void 0
+                    }
+                });
+            e.a = h
+        }
+    }
+]);
